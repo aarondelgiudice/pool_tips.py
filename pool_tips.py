3d452d6343ec88f70ecccbb40bcb08592a9dae57
Aaron Del Giudice <34919334+aarondelgiudice@users.noreply.github.com>
2018-11-18 13:06:42 -0500

    Update pool_tips.py

--- pool_tips.py
+++ pool_tips.py
@@ -1,24 +1,35 @@
-def pool_tips(hours_s=8, hours_bb=12, hours_bar=12, tips_cc=500, manager=False, wage_bb=7.5, wage=3.33, wage_m=10):
+def pool_tips(hours_s=8, hours_bar=12, tips_cc=500, manager=False, wage_bb=7.5, wage=3.33, wage_m=10):
 
-    """F(X) POOLS TIPS OVER MULTIPLE EMPLOYEES WITH DIFFERENT HOURLY PAY RATES"""
+    """ F(X) POOLS TIPS OVER MULTIPLE EMPLOYEES WITH DIFFERENT HOURLY PAY RATES """
 
     #server is paid $3.33/hr and works shorter hours than the bar staff(bartender or manager, barback)
     #bartender is paid $3.33/hr and works the same hours as the barback
     #barback is paid $7.5/hr and works the same hours as the bartender or manager
     #manager is paid $10/hr and works instead of the bartender
 
-    assert type(float(hours_s)) == float, 'hours_s is invalid'
-    assert type(float(hours_bb)) == float, 'hours_bb is invalid'
-    assert type(float(hours_bar)) == float, 'hours_bar is invalid'
-    assert type(float(tips_cc)) == float, 'tips_cc is invalid'
-    assert type(bool(manager)) == bool, 'manager is invalid'
-    assert type(float(wage_bb)) == float, 'wage_bb is invalid'
-    assert type(float(wage)) == float, 'wage is invalid'
-    assert type(float(wage_m)) == float, 'wage_m is invalid'
-    #test value types enterted into function are correct
 
-    hours_total = hours_s + hours_bar + hours_bb
+    var_list = [hours_s, hours_bar, tips_cc, manager, wage_bb, wage, wage_m]
+    #list of variables used in the function
+    var_list_nums = []
+    #empty list that will contain all numeric values used by function
+    
+    def assert_func(i):
+        """ FUNC WILL ASSERT THAT VARIABLE IS A NUMBER """
+        assert i * 0 == 0
+
+    for i in var_list:
+        assert_func(i)
+        if assert_func == True:
+            var_list_nums.append(i)
+    #for variable in var_list assert variable is a number and append it to var_list_nums
+            
+    assert type(bool(manager)) == bool, manager + ' is invalid, expected a true/false'
+    #test value types entered for manager are correct
+
+    hours_total = hours_s + ( hours_bar * 2 )
     #sums total hours of all employees, bartender hours and managers hours are the same
+    #bar hours are doubled because the bartender and barback work the same number of hours
+
 
     if manager == True:
         hours_s_adj = (wage_m-wage)*hours_s
@@ -37,11 +48,11 @@ def pool_tips(hours_s=8, hours_bb=12, hours_bar=12, tips_cc=500, manager=False, 
     if manager == True:
         tips_total_bar = (tips_adj/hours_total)*hours_bar
         tips_total_bb = (
-            (tips_adj/hours_total)*hours_bb) + hours_bar_adj
+            (tips_adj/hours_total)*hours_bar) + hours_bar_adj
     else:
         tips_total_bar = (
             (tips_adj/hours_total)*hours_bar) + hours_bar_adj
-        tips_total_bb = (tips_adj/hours_total)*hours_bb
+        tips_total_bb = (tips_adj/hours_total)*hours_bar
     #sum bar/barback tips by hours worked, adjusted for pay rate and if manager was present
 
     tips_total_bar_r = round(tips_total_bar, 2)
@@ -52,7 +63,7 @@ def pool_tips(hours_s=8, hours_bb=12, hours_bar=12, tips_cc=500, manager=False, 
     rate_s = round(
         ( ( tips_total_s_r + ( wage * hours_s ) ) / hours_s ), 2)
     rate_bb = round(
-        ( ( tips_total_bb_r + (wage_bb * hours_bb) ) / hours_bb ), 2)
+        ( ( tips_total_bb_r + (wage_bb * hours_bar) ) / hours_bar ), 2)
     #calculate hourly rate by total tips, wage, and hours worked
 
     if manager == True:
@@ -77,4 +88,4 @@ def pool_tips(hours_s=8, hours_bb=12, hours_bar=12, tips_cc=500, manager=False, 
     '\n' + 'Hourly rate: ' + str(rate))
     #display final pooled tip values for each employee
 
-pool_tips(6, 9.5, 9.5, 549.1, True)
+pool_tips(6, 9.5, 500, True)
